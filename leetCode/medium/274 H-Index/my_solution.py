f920116775b72091af1503fbf3ed94c42ae454c7
Brahmendra Bachi <hari8185008552@gmail.com>
2024-07-24 23:38:56 +0530

    add h_index

---
+++ leetCode/medium/274 H-Index/my_solution.py
@@ -0,0 +1,12 @@
+def hIndex(citations):
+    n = len(citations)
+    if n == 1:
+        return 1 if citations[0] > 0 else 0
+    citations.sort()
+    max_ind = 0
+    for i in range(n):
+        cite = citations[i]
+        n_papers = n - i
+        if n_papers <= cite:
+            return n_papers
+    return 0
