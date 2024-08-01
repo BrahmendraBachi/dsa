b5e2b4d5bd434d3ad03bde291a476ee6c5aa5e98
Brahmendra Bachi <hari8185008552@gmail.com>
2024-08-01 17:50:44 +0530

    add '209. Minimum Size Subarray Sum'

---
+++ leetCode/medium/209 Minimum Size Subarray Sum/details.md
@@ -0,0 +1,7 @@
+# question:
+    https://leetcode.com/problems/minimum-size-subarray-sum/
+
+# concepts
+    1. `Array`
+    2. `Binary` 
+    3. `Search`
---
+++ leetCode/medium/209 Minimum Size Subarray Sum/my_solution.py
@@ -0,0 +1,24 @@
+def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+    len_n = len(nums)
+    l, r = 0, 1
+    if len(nums) == 1:
+        return 0 if nums[0] < target else 1
+    min_len = None
+    if nums[l] >= target or nums[r] >= target:
+        return 1
+    slide_sum = nums[0] + nums[1]
+    while l <= r < len_n:
+        if slide_sum >= target:
+            min_len = min(min_len, r - l + 1) if min_len is not None else r - l + 1
+            slide_sum -= nums[l]
+            l = l + 1
+            if nums[l] >= target:
+                return 1
+        else:
+            r = r + 1
+            if r >= len_n:
+                break
+            if nums[r] >= target:
+                return 1
+            slide_sum += nums[r]
+    return 0 if min_len is None else min_len
