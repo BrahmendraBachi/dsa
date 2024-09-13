7d21e35855410e70a9fe1154d09345d23a0e0ad7
Brahmendra Bachi <hari8185008552@gmail.com>
2024-09-14 00:05:10 +0530

    modify/test

--- test_practice/test.py
+++ test_practice/test.py
@@ -1,50 +1,15 @@
-def minWindow(s, t):
-    chrs_dict = {}
-    curr_chrs_dict = {}
-    for ch in t:
-        if ch in chrs_dict:
-            chrs_dict[ch] += 1
-        else:
-            chrs_dict[ch] = 1
-    i = j = 0
-    min_count = None
-    min_windowed_string = ""
-    while i < len(s) or len(chrs_dict) == 0:
-        if len(chrs_dict) > 0:
-            if s[i] in chrs_dict:
-                if chrs_dict[s[i]] > 1:
-                    chrs_dict[s[i]] -= 1
-                else:
-                    del chrs_dict[s[i]]
-            else:
-                if s[i] in curr_chrs_dict:
-                    curr_chrs_dict[s[i]] += 1
-                else:
-                    curr_chrs_dict[s[i]] = 1
-            i += 1
-        else:
-            if min_count is None or min_count > (i - j):
-                min_count = i - j
-                min_windowed_string = s[j:i]
-            if s[j] in curr_chrs_dict:
-                if curr_chrs_dict[s[j]] > 1:
-                    curr_chrs_dict[s[j]] -= 1
-                else:
-                    del curr_chrs_dict[s[j]]
-            else:
-                if s[j] in chrs_dict:
-                    chrs_dict[s[j]] += 1
-                else:
-                    chrs_dict[s[j]] = 1
-            j += 1
-
-    return min_windowed_string
+def move_arr(arr, index=None):
+    print(arr)
+    if index == 0:
+        return arr[0]
+    if index is None:
+        index = 0
+    val = arr[index]
+    arr[index] = move_arr(arr, (index + 1) if index + 1 < len(arr) else 0)
+    return val
 
 
 if __name__ == "__main__":
-    input1 = "AABBECABC"
-    input2 = "ADOBECODEBANC"
-    print(minWindow(input1, "ABC"))
-    print(minWindow(input2, "ABC"))
-    print(minWindow("a", "a"))
-    print(minWindow("aa", "aaa"))
+    input1 = [1, 2, 3, 4, 5]
+    move_arr(input1)
+    print(input1)
