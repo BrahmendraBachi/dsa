ab49f62f122a3140fb0f3bda7bb2fdfa41d2613b
Brahmendra_Bachi <hari8185008552@gmail.com>
2024-10-02 19:13:24 +0530

    add/13_Roman to Integer

---
+++ leetCode/easy/solved/13 Roman to Integer/my_solution.py
@@ -0,0 +1,22 @@
+def mostFrequentEven(self, nums: List[int]) -> int:
+    nums_count = {}
+    max_count_num = None
+    max_count = 0
+    for num in nums:
+        if num % 2 == 0:
+            if num in nums_count:
+                nums_count[num] += 1
+            else:
+                nums_count[num] = 1
+            if max_count_num is None:
+                max_count = nums_count[num]
+                max_count_num = num
+            elif max_count == nums_count[num]:
+                max_count_num = min(num, max_count_num)
+            elif max_count < nums_count[num]:
+                max_count_num = num
+                max_count = nums_count[num]
+            else:
+                pass
+
+    return -1 if max_count_num is None else max_count_num
