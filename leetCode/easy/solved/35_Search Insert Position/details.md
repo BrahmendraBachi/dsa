865e939489d6ce51e89e3776b6463c4c2c6b1c34
Brahmendra_Bachi <hari8185008552@gmail.com>
2024-10-04 22:05:46 +0530

    add/35_Search Insert Position

---
+++ leetCode/easy/solved/35_Search Insert Position/details.md
@@ -0,0 +1,11 @@
+# question
+https://leetcode.com/problems/search-insert-position/description/
+
+# **My-solution**
+
+### _time-complexity_: `O(nlogn)`
+### _space-complexity_: `O(1)`
+
+
+
+# topics: `Arrays`, `Binary Search`'
---
+++ leetCode/easy/solved/35_Search Insert Position/my_solution.py
@@ -0,0 +1,9 @@
+def searchInsert(self, nums, target):
+    def binary_search(left, right):
+        if left > right:
+            return left
+        m = (left + right) // 2
+        if target == nums[m]:
+            return m
+        return binary_search(m + 1, right) if target > nums[m] else binary_search(left, m - 1)
+    return binary_search(0, len(nums) - 1)
