90b357b2b9fe9e5688a3ddca12c4012d0d2fa5fe
Brahmendra_Bachi <hari8185008552@gmail.com>
2024-10-31 17:35:42 +0530

    update/linkedList test

--- concepts/linkedlist/testLinkedList.py
+++ concepts/linkedlist/testLinkedList.py
@@ -1,30 +1,48 @@
 from dsa.concepts.linkedlist.linkedlist import LinkedList
 
-def swapNodesInPairs(linkedList: LinkedList):
-    head = linkedList.returnHead()
-    if head is None or head.next is None:
+def reverseNodesInKGroups(linkedList: LinkedList, k: int):
+    n = linkedList.get_length()
+    head = start = linkedList.returnHead()
+    if n < k:
         return head
-    ptr1 = head
-    start_head = ptr2 = head.next
-    prev = None
-    recursivelySwapNodes(prev, ptr1, ptr2)
-    return start_head
+    start_head = None
+    currNode = head
+    prevGroupNode = None
+    for i in range(n // k):
+        count = 1
+        prevNode = None
+        while count <= k:
+            next_node = currNode.next
+            currNode.next = prevNode
 
-def recursivelySwapNodes(prev, ptr1, ptr2):
-    if not (ptr1 and ptr2):
-        return
-    temp = ptr2.next
-    ptr2.next = ptr1
-    ptr1.next = temp
-    if prev:
-        prev.next = ptr2
-    prev = ptr1
-    ptr1 = ptr1.next
-    if ptr1:
-        ptr2 = ptr1.next
-    return recursivelySwapNodes(prev, ptr1, ptr2)
+            prevNode = currNode
+            currNode = next_node
+            count += 1
+        if start_head is None:
+            start_head = prevNode
+        if prevGroupNode:
+            prevGroupNode.next = prevNode
+        prevGroupNode = start
+        start = currNode
+    if currNode:
+        prevGroupNode.next = currNode
+    return start_head if start_head else currNode
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
-    linkedList1 = LinkedList([1, 2, 3, 4, 5, 6])
-    node = swapNodesInPairs(linkedList1)
-    LinkedList(start_head=node).print_elements()
+    linkedList1 = LinkedList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+    k1 = 4
+    # head = reverseNodesInKGroups(linkedList1, k1)
+    # LinkedList(start_head=head).print_elements()
+
+    linkedList2 = LinkedList([1, 2, 3])
+    k2 = 4
+    head = reverseNodesInKGroups(linkedList2, k2)
+    LinkedList(start_head=head).print_elements()
+    # LinkedList(star).print_elements()
