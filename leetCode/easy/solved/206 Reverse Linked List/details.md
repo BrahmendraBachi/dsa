a3239d3ae7b8fdda2009e2d377e53f12be7db4bd
Brahmendra_Bachi <hari8185008552@gmail.com>
2024-10-31 13:25:29 +0530

    add/206_Reverse Linked List

---
+++ leetCode/easy/solved/206 Reverse Linked List/details.md
@@ -0,0 +1,10 @@
+# question
+https://leetcode.com/problems/reverse-linked-list/description/
+
+# **My-solution**
+
+### _time-complexity_: `O(n)`
+### space-complexity: `O(1) --> Iterative` && `O(n) --> Recursive`
+
+
+# topics: `LinkedList`, `Recursion`'
---
+++ leetCode/easy/solved/206 Reverse Linked List/my_solution_2.py
@@ -0,0 +1,10 @@
+# Recursive
+def reverseList(self, head):
+    return self.recursiveReverse(None, head)
+
+def recursiveReverse(self, prev, curr):
+    if not curr:
+        return prev
+    temp = curr.next
+    curr.next = prev
+    return self.recursiveReverse(curr, temp)
