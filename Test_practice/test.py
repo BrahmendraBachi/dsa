33a6b08ef8bbba3115b8e970a8ae44e23e7b20fc
Brahmendra_Bachi <hari8185008552@gmail.com>
2025-01-18 15:12:07 +0530

    modify_HackerRank/dir_name

--- Test_practice/test.py
+++ Test_practice/test.py
@@ -1,54 +1,23 @@
-def getWays(n, coins, start=0, dp=None):
-    if dp is None:
-        dp = {}
-    hashKey = str(n) + " " + str(start)
-    if hashKey in dp:
-        return dp[hashKey]
-    if n == 0:
-        return 1
-    count = 0
-    for i in range(start, len(coins)):
-        if n - coins[i] >= 0:
-            count += getWays(n - coins[i], coins, i, dp)
-            continue
-        break
-    dp[hashKey] = count
-    return count
-
-# def getWays(n, c):
-#     # Write your code here
-#     c.sort()
-
-#     return recursive_count(n, c)
-
-
-def recursive_count(n, c, start=0, dp=None):
-    if dp is None:
-        dp = {}
-    hashKey = str(n) + " " + str(start)
-    if hashKey in dp:
-        return dp[hashKey]
-    if n == 0:
-        return 1
-    count = 0
-    for i in range(start, len(c)):
-        if n - c[i] >= 0:
-            count += recursive_count(n - c[i], c, i, dp)
-            continue
-        break
-    dp[hashKey] = count
-    return count
-
+def find_pages(arr, k):
+    def allocate(rem_arr, rem_k, start=1, allocation_arr=None):
+        end = len(rem_arr) - rem_k + 1
+        if allocation_arr is None:
+            allocation_arr = []
+        if rem_k == 1:
+            return [max(allocation_arr + [sum(rem_arr)])]
+        allocations = []
+        for i in range(1, end + 1):
+            curr_sum = sum(rem_arr[:i])
+            allocation_arr.append(curr_sum)
+            allocations += allocate(rem_arr[i:], rem_k - 1, i, allocation_arr)
+            allocation_arr.pop()
+        return allocations
+    all_allocations = allocate(arr, k)
+    return min(all_allocations)
 
 if __name__ == "__main__":
-    input1 = [i for i in range(1, 26)]
-    # for i in input1:
-    #     print(i, end=" ")
-    # print(input1)
-    print(getWays(400, input1))
-
-    input2 = [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,62,64,66,68,70,72,74,76,78,80,82,84,86,88,90,92,94,96,98,100,102,104,106,108,110,112,114,116,118,120,122,124,126,128,130,132,134,136,138,140,142,144,146,148,150,152,154,156,158,160,162,164,166,168,170,172,174,176,178,180,182,184,186,188,190,192,194,196,198,200,202,204,206,208,210,212,214,216,218,220,222,224,226,228,230,232,234,236,238,240,242,244,246,248,250,252,254,256,258,260,262,264,266,268,270,272,274,276,278,280,282,284,286,288,290,292,294,296,298,300,302,304,306,308,310,312,314,316,318,320,322,324,326,328,330,332,334,336,338,340,342,344,346,348,350,352,354,356,358,360,362,364,366,368,370,372,374,376,378,380,382,384,386,388,390,392,394,396,398,400,402,404,406,408,410,412,414,416,418,420,422,424,426,428,430,432,434,436,438,440,442,444,446,448,450,452,454,456,458,460,462,464,466,468,470,472,474,476,478,480,482,484,486,488,490,492,494,496,498,500,502,504,506,508,510,512,514,516,518,520,522,524,526,528,530,532,534,536,538,540,542,544,546,548,550,552,554,556,558,560,562,564,566,568,570,572,574,576,578,580,582,584,586,588,780,936,1170,1560,2340,4680]
-    for i in input2:
-        print(i, end=" ")
-    print("\n", len(input2))
-    # print(getWays(4681, input2))
+    input1 = ([3, 1, 1], 2)
+    input2 = ([3, 1, 2, 1, 6], 3)
+    print(find_pages(input2[0], input2[1]))
+    # print(find_pages(list(range(1, 31)), 10))
+    # input3 =
