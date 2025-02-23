6eac8a6b5e03e9fed376569a733483caf459abcb
Brahmendra_Bachi <hari8185008552@gmail.com>
2025-02-23 15:03:11 +0530

    modify/230 Kth Smallest Element in a BST

--- Questions/LeetCode/medium/solved/230 Kth Smallest Element in a BST/my_solution.py
+++ Questions/LeetCode/medium/solved/230 Kth Smallest Element in a BST/my_solution.py
@@ -1,18 +1,26 @@
-def kthSmallest(self, root, k):
-    countAndValue = {"count": 0, "val": None}
+from typing import Optional
 
-    def inOrderTraverse(node):
-        if countAndValue["val"]:
-            return
-        if node.left:
-            inOrderTraverse(node.left)
-        countAndValue["count"] += 1
-        if countAndValue["count"] == k:
-            countAndValue["val"] = node.val
-            return
-        if node.right:
-            inOrderTraverse(node.right)
-        return
 
-    inOrderTraverse(root)
-    return countAndValue["val"]
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def __init__(self):
+        self.count = 0
+
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        if root.left:
+            val = self.kthSmallest(root.left, k)
+            if val is not None:
+                return val
+        self.count += 1
+        if self.count == k:
+            return root.val
+        if root.right:
+            val = self.kthSmallest(root.right, k)
+            if val is not None:
+                return val
