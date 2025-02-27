2765b91853e6b6b3beefa6966595f1d5db68380c
BrahmendraBachi <hari8185008552@gmail.com>
2025-02-27 06:21:06 +0530

    modify_Concept/graph

--- Concepts/DataTypes/Graphs/Directional_Graphs/graph.py
+++ Concepts/DataTypes/Graphs/Directional_Graphs/graph.py
@@ -1,12 +1,15 @@
 class Graph:
 
-    def __init__(self, edges=None):
+    def __init__(self, edges=None, graph=None):
         self.edges = edges
-        self.graph = self.__get_graph()
+        self.graph = graph
+
+        if self.graph is None and self.edges is not None:
+            self.graph = self.__get_graph()
 
     def print_graph(self):
-        for node, adj_nodes in self.graph.items():
-            print(f"{node}, {adj_nodes}")
+        for node, adj_node in self.graph.items():
+            print(f"{node}, {adj_node}")
 
     def dfs(self, source):
         stack = [source]
@@ -33,8 +36,39 @@ class Graph:
                 for adj_node in self.graph[node]:
                     graph_queue.append(adj_node)
 
+    def check_if_path_exists_dfs(self, source, destination):
+        stack = [source]
+        while len(stack):
+            node = stack.pop()
+            if node == destination:
+                return True
+            if node not in self.graph:
+                continue
+            for adj_node in self.graph[node]:
+                stack.append(adj_node)
+        return
 
+    def check_if_path_exists_dfs_recursive(self, source, destination):
+        if source == destination:
+            return True
+        if source not in self.graph:
+            return False
+        for adj_node in self.graph[source]:
+            if adj_node in self.graph:
+                if self.check_if_path_exists_dfs_recursive(adj_node, destination):
+                    return True
+        return False
 
+    def check_if_path_exists_bfs(self, source, destination):
+        nodes_queue = [source]
+        while len(nodes_queue):
+            node, nodes_queue = nodes_queue[0], nodes_queue[1:]
+            if node == destination:
+                return True
+            if node in self.graph:
+                for adj_node in self.graph[node]:
+                    nodes_queue.append(adj_node)
+        return False
 
     def __get_graph(self):
         graph = {}
@@ -47,26 +81,25 @@ class Graph:
 
 
 def main():
-    edges = [
-        ("a", "b"),
-        ("a", "c"),
-        ("c", "e"),
-        ("b", "d"),
-        ("f", "d")
-    ]
+    graph_dict = {
+        "f": ["i", "g"],
+        "g": ["h"],
+        "h": [],
+        "i": ["g", "k"],
+        "j": ["i"],
+        "k": []
+    }
 
-    graph = Graph(edges)
+    graph = Graph(graph=graph_dict)
     # graph.print_graph()
+    # print(f"is path exists from 'f' to 'g': {graph.check_if_path_exists_dfs('f', 'g')}")
+    # print(f"is path exists from 'f' to 'g': {graph.check_if_path_exists_dfs('j', 'h')}")
 
-    # DFS
-    graph.dfs("a")
-    print()
-    graph.dfs_recursive("a")
-    print()
+    # print(f"is path exists from 'f' to 'g': {graph.check_if_path_exists_dfs_recursive('f', 'k')}")
+    # print(f"is path exists from 'f' to 'g': {graph.check_if_path_exists_dfs_recursive('i', 'j')}")
 
-    graph.bfs("a")
-
-
+    print(f"is path exists from 'f' to 'g': {graph.check_if_path_exists_bfs('f', 'k')}")
+    print(f"is path exists from 'f' to 'g': {graph.check_if_path_exists_bfs('i', 'j')}")
 
 
 if __name__ == "__main__":
