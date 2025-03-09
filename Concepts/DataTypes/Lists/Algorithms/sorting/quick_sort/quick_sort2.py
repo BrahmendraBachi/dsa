50436454bcdbacbe4bd3a4ebfc8a21d7f2c9ae13
BrahmendraBachi <hari8185008552@gmail.com>
2025-03-09 14:11:19 +0530

    modify_Concept/quick_sort

--- Concepts/DataTypes/Lists/Algorithms/sorting/quick_sort/quick_sort2.py
+++ Concepts/DataTypes/Lists/Algorithms/sorting/quick_sort/quick_sort2.py
@@ -22,7 +22,44 @@ def quick_sort(arr, low=0, high=None):
     quick_sort(arr, pivot + 1, high)
 
 
+def run_test_case(inp, out):
+    _inp = inp.copy()
+    quick_sort(inp)
+
+    try:
+        for i in range(len(inp)):
+            assert inp[i] == out[i]
+            pass
+        print("Test Passed!")
+    except AssertionError:
+        RED = '\033[31m'
+        RESET = '\033[0m'
+        print(
+            RED + f"Test Failed! Input: {RESET} {_inp} {RED}, Expected: {RESET}{out}{RED}, Got: {RESET}{inp}"
+        )
+
+    except Exception as e:
+        RED = '\033[31m'
+        RESET = '\033[0m'
+        print(RED + f"An unexpected error occurred: {e}" + RESET)
+
+
+def main():
+    test_cases = [
+        {"input": [], "output": []},
+        {"input": [5], "output": [5]},
+        {"input": [1, 2, 3, 4, 5], "output": [1, 2, 3, 4, 5]},
+        {"input": [5, 4, 3, 2, 1], "output": [1, 2, 3, 4, 5]},
+        {"input": [1, 3, 5, 2, 3, 1], "output": [1, 1, 2, 3, 3, 5]},
+        {"input": [2, 2, 2, 2, 2], "output": [2, 2, 2, 2, 2]},
+        {"input": [-5, 2, -1, 0, 3], "output": [-5, -1, 0, 2, 3]},
+        {"input": [0, -3, 5, -1, 2], "output": [-3, -1, 0, 2, 5]},
+        {"input": [1, 2, 3, 5, 4], "output": [1, 2, 3, 4, 5]}
+    ]
+
+    for test_case in test_cases:
+        run_test_case(test_case["input"], test_case["output"])
+
+
 if __name__ == "__main__":
-    input1 = [9, 1, 3, 2, 6, 7, 4]
-    quick_sort(input1)
-    print(input1)
+    main()
