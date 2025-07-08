9cb4347fc39d5a0bfb3283a3f08fc2ff510d143c
Brahmendra_Bachi <hari8185008552@gmail.com>
2025-07-08 10:21:20 +0530

    modify_algorithm/binary_search

--- Concepts/DataTypes/Lists/Algorithms/search/binary_search/solution.py
+++ Concepts/DataTypes/Lists/Algorithms/search/binary_search/solution.py
@@ -1,18 +1,35 @@
+from dsa.Concepts.DataTypes.Lists.Algorithms.search.binary_search.test_cases import test_cases
+
+
 def binary_search(arr, target, l=0, r=None):
-    n = len(arr)
     if r is None:
-        r = n - 1
-    m = (l + r) // 2
+        r = len(arr) - 1
     if l > r:
         return -1
-    if arr[m] == target:
-        return m
+    m = (l + r) // 2
     if arr[m] < target:
         return binary_search(arr, target, m + 1, r)
-    return binary_search(arr, target, l, m - 1)
+    elif arr[m] > target:
+        return binary_search(arr, target, l, m - 1)
+    else:
+        return m
 
 
 if __name__ == "__main__":
-    arr1 = [5, 25, 75]
-    print()
+    try:
+
+        for test_case in test_cases:
+            act_out = binary_search(test_case["inp_arr"], test_case["tar_val"])
+            if act_out != test_case["exp_out"]:
+                raise Exception(
+                    f"input_arr: {test_case['inp_arr']},\n"
+                    f"exp_out: {test_case['exp_out']},\n"
+                    f"actual output: {act_out}"
+                )
+
+
+        print("Passed all testCases")
+    except Exception as err:
+        print(err)
+
 
---
+++ Concepts/DataTypes/Lists/Algorithms/search/binary_search/test_cases.py
@@ -0,0 +1,87 @@
+test_cases = [
+    {
+        "inp_arr": [],
+        "tar_val": 5,
+        "exp_out": -1
+    },
+    {
+        "inp_arr": [5],
+        "tar_val": 5,
+        "exp_out": 0
+    },
+    {
+        "inp_arr": [5],
+        "tar_val": 3,
+        "exp_out": -1
+    },
+    {
+        "inp_arr": [1, 3],
+        "tar_val": 1,
+        "exp_out": 0
+    },
+    {
+        "inp_arr": [1, 3],
+        "tar_val": 3,
+        "exp_out": 1
+    },
+    {
+        "inp_arr": [1, 3],
+        "tar_val": 2,
+        "exp_out": -1
+    },
+    {
+        "inp_arr": [1, 3, 5],
+        "tar_val": 3,
+        "exp_out": 1
+    },
+    {
+        "inp_arr": [1, 3, 5, 7],
+        "tar_val": 5,
+        "exp_out": 2
+    },
+    {
+        "inp_arr": [10, 20, 30],
+        "tar_val": 5,
+        "exp_out": -1
+    },
+    {
+        "inp_arr": [10, 20, 30],
+        "tar_val": 40,
+        "exp_out": -1
+    },
+    {
+        "inp_arr": [10, 20, 30],
+        "tar_val": 25,
+        "exp_out": -1
+    },
+    {
+        "inp_arr": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        "tar_val": 9,
+        "exp_out": 9
+    },
+    {
+        "inp_arr": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        "tar_val": 0,
+        "exp_out": 0
+    },
+    {
+        "inp_arr": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        "tar_val": 10,
+        "exp_out": -1
+    },
+    {
+        "inp_arr": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        "tar_val": -1,
+        "exp_out": -1
+    },
+    {
+        "inp_arr": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        "tar_val": 5,
+        "exp_out": 5
+    },
+    {
+        "inp_arr": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        "tar_val": 6,
+        "exp_out": 6
+    }
+]
