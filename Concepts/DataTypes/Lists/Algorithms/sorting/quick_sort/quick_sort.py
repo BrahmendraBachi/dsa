ef854472a2bcebd9e8a05aded0a6f26da176ae82
Brahmendra_Bachi <hari8185008552@gmail.com>
2025-07-16 11:22:52 +0530

    modify_lists/quick_sort

--- Concepts/DataTypes/Lists/Algorithms/sorting/quick_sort/quick_sort.py
+++ Concepts/DataTypes/Lists/Algorithms/sorting/quick_sort/quick_sort.py
@@ -1,28 +1,38 @@
+from Concepts.DataTypes.Lists.Algorithms.sorting.insertion_sort.insertion_sort import insertion_sort
+from Concepts.DataTypes.Lists.Algorithms.sorting.sorting_helpers import is_sorted
+from Concepts.DataTypes.Lists.Algorithms.sorting.test_cases import test_cases
+
+
 def partition(arr):
+    # consider last element as our pivot
+    i = -1
+    j = 0
     pivot = len(arr) - 1
-    i, j = -1, 0
+
     while j < pivot:
         if arr[j] < arr[pivot]:
             i += 1
             arr[i], arr[j] = arr[j], arr[i]
         j += 1
 
-    arr[i + 1], arr[pivot] = arr[pivot], arr[i + 1]
+    i += 1
+    arr[i], arr[pivot] = arr[pivot], arr[i]
 
-    return i + 1
+    return i
 
 
 def quick_sort(arr):
-    if len(arr) == 0 or len(arr) == 1:
+    if len(arr) <= 1:
         return arr
-    par_ind = partition(arr)
 
-    partition_ele = arr[par_ind]
-    left_arr = quick_sort(arr[:par_ind])
-    right_arr = quick_sort(arr[par_ind + 1:])
+    p_ind = partition(arr)
 
-    return left_arr + [partition_ele] + right_arr
+    left_arr = arr[:p_ind]
+    right_arr = arr[p_ind + 1:]
 
+    arr = quick_sort(left_arr) + [arr[p_ind]] + quick_sort(right_arr)
+
+    return arr
 
 def run_test_case(inp, out):
     res = quick_sort(inp)
@@ -46,20 +56,18 @@ def run_test_case(inp, out):
 
 
 def main():
-    test_cases = [
-        {"input": [], "output": []},
-        {"input": [5], "output": [5]},
-        {"input": [1, 2, 3, 4, 5], "output": [1, 2, 3, 4, 5]},
-        {"input": [5, 4, 3, 2, 1], "output": [1, 2, 3, 4, 5]},
-        {"input": [1, 3, 5, 2, 3, 1], "output": [1, 1, 2, 3, 3, 5]},
-        {"input": [2, 2, 2, 2, 2], "output": [2, 2, 2, 2, 2]},
-        {"input": [-5, 2, -1, 0, 3], "output": [-5, -1, 0, 2, 3]},
-        {"input": [0, -3, 5, -1, 2], "output": [-3, -1, 0, 2, 5]},
-        {"input": [1, 2, 3, 5, 4], "output": [1, 2, 3, 4, 5]}
-    ]
+    try:
+        for test_case in test_cases:
+            copied_test_case = test_case["inp_arr"]
+            insertion_sort(copied_test_case)
+            if not is_sorted(copied_test_case.copy()):
+                raise Exception(f"input_arr: {test_case['inp_arr']} \n",
+                                f"Expected to be sorted, got this output: {copied_test_case}")
+        print("Passed all testCases")
 
-    for test_case in test_cases:
-        run_test_case(test_case["input"], test_case["output"])
+    except Exception as err:
+        print(err)
+
 
 
 if __name__ == "__main__":
